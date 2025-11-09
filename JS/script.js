a1fd68822865bd17f78429891dc0ab2f5a2ea6bb
nayaralenzi <naay.lenzi@gmail.com>
2025-11-09 20:56:54 -0300

    Construção do html da página inicial

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+/*function calculeArea (base, altura) { 
+    return base * altura 
+}
+
+let base = prompt("Digite o valor da base: ");
+let altura = prompt("Digite o valor para a altura: ");
+
+let resultado = calculeArea (base, altura);
+console.log("A área calculada é: ", resultado);
+alert("A área calculada é: " + resultado);
+*/
+
+funtion
